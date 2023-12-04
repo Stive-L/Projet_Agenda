bab5199446aa40e4b0891559e0ec9a6e9831eff0
Yeto4president <jonathan.ayeto@gmail.com>
2023-12-04 01:00:15 +0100

    modif

--- list.c
+++ list.c
@@ -101,14 +101,16 @@ void insertion_en_ordre(t_d_list *list, int level, int value) {
     newCell->next[level] = current->next[level];
     current->next[level] = newCell;
 }
-t_d_cell *recherche_0(t_d_list *list, int value) {
+
+int recherche_0(t_d_list *list, int value) {
     t_d_cell *current = list->head[0];
     while (current != NULL && current->value != value) {
         current = current->next[0];
     }
-    return current;
+    return (current != NULL) ? 1 : 0; // Retourne 1 si la valeur est trouvée, sinon 0
 }
-t_d_cell *recherche_du_plus_haut(t_d_list *list, int value) {
+
+int recherche_du_plus_haut(t_d_list *list, int value) {
     int currentLevel = list->max_levels - 1;
 
     t_d_cell *current = list->head[currentLevel];
@@ -120,7 +122,7 @@ t_d_cell *recherche_du_plus_haut(t_d_list *list, int value) {
 
         // Si la valeur est trouvée au niveau actuel
         if (current != NULL && current->value == value) {
-            return current; // Retournez la cellule correspondante
+            return 1; // Retournez 1 si la valeur est trouvée
         }
 
         // Si la valeur n'est pas trouvée, descendez d'un niveau
@@ -130,5 +132,5 @@ t_d_cell *recherche_du_plus_haut(t_d_list *list, int value) {
         }
     }
 
-    return NULL;
+    return 0; // Retournez 0 si la valeur n'est pas trouvée
 }
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "list.h"
 #include "cell.h"
+#include "timer.h"
+#include <stdlib.h>
 
 int main() {
     printf("Hello, World!\n");
@@ -10,6 +12,7 @@ int main() {
     insertion_en_tete(list,cell);
     display_all_levels(list);
     // Niveau 0
+   /*
     insertion_en_ordre(list,0,18);
     insertion_en_ordre(list,0,25);
     insertion_en_ordre(list,0,31);
@@ -44,7 +47,7 @@ int main() {
     insertion_en_ordre(list,4,59);
     display_all_levels(list);
     cell = recherche_du_plus_haut(list, 25);
-
+/*
 
         /*
          cell = recherche_0(list, 18);
@@ -57,6 +60,41 @@ int main() {
         insertion_en_tete(list,cell1);
         display_all_levels(list);
         */
-    return 0;
+    int initialLevel = 7;
+    int finalLevel = 15;
 
+    // Initialisation du générateur de nombres aléatoires
+    srand(time(NULL));
+
+    // Boucle sur tous les niveaux à tester
+    for (int currentLevel = initialLevel; currentLevel <= finalLevel; currentLevel++) {
+        printf("Niveau testé: %d\n", currentLevel);
+
+        // Création de la liste à niveaux
+        t_d_list *nv = create_multi_level_list(currentLevel);
+
+        // Démarrage du timer pour la recherche au niveau 0
+        startTimer();
+        for (int i = 0; i < 10000; i++) {  // 10 000 recherches au hasard
+            int randomValue = rand() % (currentLevel * 10);
+            recherche_0(list, randomValue);
+        }
+        stopTimer();
+        printf("Search time at level 0: %s\n", getTimeAsString());
+
+        // Démarrage du timer pour la recherche multi-niveaux
+        startTimer();
+        for (int i = 0; i < 10000; i++) {  // 10 000 recherches au hasard
+            int randomValue = rand() % (currentLevel * 10);
+            recherche_du_plus_haut(list, randomValue);
+        }
+        stopTimer();
+        printf("Search time at multi-levels: %s\n", getTimeAsString());
+
+        // Libération de la mémoire de la liste
+        // (vous devrez implémenter cette fonction)
+        // free_multi_level_list(list);
+    }
+
+    return 0;
 }
