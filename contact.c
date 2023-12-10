843751f9c0658631ae8c845ff023ac2cce27cf30
Stive <zhuo-chan-stive.lee@efrei.net>
2023-12-10 22:40:06 +0100

    Finalisation du projet (last push)

--- contact.c
+++ contact.c
@@ -3,12 +3,41 @@
 //
 
 #include "contact.h"
+t_d_listAgenda * createAgenda(){
+    t_d_listAgenda *Agenda = malloc(sizeof(t_d_listAgenda));
+    Agenda->max_levels = 4;
+    Agenda->head = malloc(4 * sizeof(Agenda_cell));
+    return Agenda;
 
-char *scanString(void) {
-    char nom_prenom[100];
-    fgets(nom_prenom,100,stdin);
-    nom_prenom[strcspn(nom_prenom, "\n")] = '\0'; // On enleve le caractère retour à la ligne lors de l'utilisation de fgets
-    char *chaine = malloc(strlen(nom_prenom));
-    strncpy(chaine, nom_prenom, strlen(nom_prenom) + 1); // Copie de nom_prenom à chaine
-    return chaine;
 }
+void afficherRendezVous(t_d_listAgenda *agenda, char *nom) {
+    // Si l'agenda est vide alors on affiche rien
+    if (agenda == NULL) {
+        printf("L'agenda est vide.\n");
+        return;
+    }
+    // On parcourt tous les cellules de l'agenda au niveau 0 car au niveau 0 on a tous les rendez-vous
+    Agenda_cell *current = agenda->head[0];
+    while (current != NULL) {
+        if (strcmp(current->contact.nom, nom) == 0 ) { // On compare le nom de la cellule avec le nom qu'on cerche
+            printf("Rendez-vous: %d/%d/%d à %d:%d, Durée: %d heures %d minutes, Objet: %s\n", // On affiche le rdv en question
+                   current->Rdv.jour, current->Rdv.mois, current->Rdv.annee,
+                   current->Rdv.heure, current->Rdv.minute,
+                   current->Rdv.dureeHeure, current->Rdv.dureeMinute,
+                   current->Rdv.objet);
+        }
+        current = *(current->next);
+    }
+}
+void createRdv(t_d_listAgenda * agenda){
+    return;
+}
+void supprRdv(t_d_listAgenda * agenda){
+    return;
+}
+void createContact(t_d_listAgenda * agenda){
+    return;
+}
+
+
+
--- contact.h
+++ contact.h
@@ -4,56 +4,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
 
 #ifndef PROJET_AGENDA_CONTACT_H
 #define PROJET_AGENDA_CONTACT_H
 
-char *scanString(void);
-
+// Structure pour un contact
 typedef struct Contact {
     char* nom;
     char* prenom;
 } Contact;
 
-typedef struct {
-    int jour;
-    int mois;
-    int annee;
-} Date;
-
-// Structure pour l'heure
-typedef struct {
-    int heure;
-    int minute;
-} Temps;
 
 // Structure pour un rendez-vous
 typedef struct RendezVous {
-    Date date;
-    Temps heure_debut;
-    Temps duree;
+    int jour;
+    int mois;
+    int annee;
+    int heure;
+    int minute;
+    int dureeHeure;
+    int dureeMinute;
     char* objet;
-    struct RendezVous* suivant;
 } RendezVous;
 
 
-typedef struct rdv_list {
-    RendezVous *tete;
-}rdv_list;
-
-
+// Structure pour entréeAgenda
 typedef struct EntreeAgenda {
     Contact contact;
-    rdv_list  listeRendezVous;  // Liste simplement chaînée de rendez-vous
-    struct s_d_cell **next;
-} Agenda_cell, *p_agenda_cell;
+    RendezVous Rdv;
+    struct EntreeAgenda **next; // Liste simplement chaînée de rendez-vous
+} Agenda_cell;
 
 
+// Structure pour liste à niveau d'un Agenda
+typedef struct s_d_listagenda {  // Liste à niveaux qui stock les entrees d'agenda
+    int max_levels;             // Nombre maximum à 4 de niveaux dans la liste
+    Agenda_cell ** head;      // Tableau de pointeurs vers la tête de chaque niveau agenda
+} t_d_listAgenda;
 
-typedef struct s_d_listagenda {  // List à niveaux qui stock les entrees d'agenda
-    int max_levels;             // Nombre maximum de niveaux dans la liste
-    Agenda_cell ** head;      // Tableau de pointeurs vers la tête de chaque niveau
-} s_d_listagenda;
+t_d_listAgenda * createAgenda();
+void afficherRendezVous(t_d_listAgenda *, char *);
+void createContact(t_d_listAgenda *);
+void createRdv(t_d_listAgenda *);
+void supprRdv(t_d_listAgenda *);
+
 
 #endif //PROJET_AGENDA_CONTACT_H
